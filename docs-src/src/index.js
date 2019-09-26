0b7de6f88afb48b5e152b439086b37a4e8083363
jutunen <u-jussi@suomi24.fi>
2019-09-26 14:30:31 +0300

    fixed linter errors

--- docs-src/src/index.js
+++ docs-src/src/index.js
@@ -1,5 +1,5 @@
 import { parseChord, chordRendererFactory } from '../../src/index';
-import {Pianokeys} from 'custom-piano-keys'
+import 'custom-piano-keys';
 
 const renderChordNormalized = chordRendererFactory();
 const renderChordShort = chordRendererFactory({ useShortNamings: true });
@@ -25,44 +25,44 @@ let rootNoteIndex;
 let octaveCount;
 
 function myReducer(total, item) {
-    return total + " " + item;
+	return total + ' ' + item;
 }
 
 chordInput.addEventListener('keyup', () => {
 
-  if(pianokeysDiv.firstChild) {
-    pianokeysDiv.firstChild.remove()
-  }
+	if(pianokeysDiv.firstChild) {
+		pianokeysDiv.firstChild.remove();
+	}
 
-  input = chordInput.value;
-  chord = parseChord(input);
-  if(chord) {
-    rootNoteIndex = rootNotes.indexOf(chord.normalized.rootNote)
-    if(rootNoteIndex !== -1) {
-      mappedNotes = chord.normalized.semitones.map(x => x + 1 + rootNoteIndex)
-      octaveCount = 1
-      if(mappedNotes.some(x => x > 12)) {
-        octaveCount = 2
-      }
-      markedKeys = mappedNotes.reduce(myReducer,"").trim()
-      pianokeys = document.createElement("custom-piano-keys")
-      pianokeys.setAttribute("marked-keys", markedKeys)
-      pianokeys.setAttribute("oct-count", octaveCount)
-      pianokeysDiv.appendChild(pianokeys)
-    }
-  }
+	input = chordInput.value;
+	chord = parseChord(input);
+	if(chord) {
+		rootNoteIndex = rootNotes.indexOf(chord.normalized.rootNote);
+		if(rootNoteIndex !== -1) {
+			mappedNotes = chord.normalized.semitones.map(x => x + 1 + rootNoteIndex);
+			octaveCount = 1;
+			if(mappedNotes.some(x => x > 12)) {
+				octaveCount = 2;
+			}
+			markedKeys = mappedNotes.reduce(myReducer,'').trim();
+			pianokeys = document.createElement('custom-piano-keys');
+			pianokeys.setAttribute('marked-keys', markedKeys);
+			pianokeys.setAttribute('oct-count', octaveCount);
+			pianokeysDiv.appendChild(pianokeys);
+		}
+	}
 
-  symbolNormalized = (!chord) ? '' : renderChordNormalized(chord);
-  symbolShort = (!chord) ? '' : renderChordShort(chord);
+	symbolNormalized = (!chord) ? '' : renderChordNormalized(chord);
+	symbolShort = (!chord) ? '' : renderChordShort(chord);
 
-  if (!chord) {
-    outputDetails.classList.add('hidden');
-  } else {
-    outputDetails.classList.remove('hidden');
-  }
+	if (!chord) {
+		outputDetails.classList.add('hidden');
+	} else {
+		outputDetails.classList.remove('hidden');
+	}
 
-  normalized.textContent 		= (!chord) ? '-' : symbolNormalized;
-  normalizedShort.textContent = (symbolNormalized === symbolShort) ? '' : 'alternate: ' + symbolShort;
-  intervals.textContent 		= (!chord) ? '-' : chord.normalized.intervals.join('-');
-  json.textContent 			= (!chord) ? '-' : JSON.stringify(chord, null, 4);
+	normalized.textContent 		= (!chord) ? '-' : symbolNormalized;
+	normalizedShort.textContent = (symbolNormalized === symbolShort) ? '' : 'alternate: ' + symbolShort;
+	intervals.textContent 		= (!chord) ? '-' : chord.normalized.intervals.join('-');
+	json.textContent 			= (!chord) ? '-' : JSON.stringify(chord, null, 4);
 });
