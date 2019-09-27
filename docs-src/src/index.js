11ed5ef3b9ed9f3b5605128c0594770ce14372ee
Christophe Noël <touffi51@gmail.com>
2019-09-27 09:32:07 +0200

    feat(demo): Moved Custom Piano Key integration into a dedicated Showcase section

--- docs-src/src/index.js
+++ docs-src/src/index.js
@@ -1,5 +1,3 @@
-/*eslint complexity: ["error", 12]*/
-
 import { parseChord, chordRendererFactory } from '../../src/index';
 import 'custom-piano-keys';
 
@@ -9,7 +7,6 @@ const renderChordShort = chordRendererFactory({ useShortNamings: true });
 const chordInput = document.getElementById('chordInput');
 
 const normalized = document.querySelector('[data-output="normalized"]');
-const pianokeysDiv = document.querySelector('[data-output="pianokeys"]');
 const normalizedShort = document.querySelector('[data-output="normalizedShort"]');
 const intervals = document.querySelector('[data-output="intervals"]');
 const json = document.querySelector('[data-output="json"]');
@@ -19,6 +16,34 @@ let input;
 let chord;
 let symbolNormalized;
 let symbolShort;
+
+chordInput.addEventListener('keyup', () => {
+	input = chordInput.value;
+	chord = parseChord(input);
+
+	symbolNormalized = (!chord) ? '' : renderChordNormalized(chord);
+	symbolShort = (!chord) ? '' : renderChordShort(chord);
+
+	if (!chord) {
+		outputDetails.classList.add('hidden');
+	} else {
+		outputDetails.classList.remove('hidden');
+	}
+
+	normalized.textContent 		= (!chord) ? '-' : symbolNormalized;
+	normalizedShort.textContent = (symbolNormalized === symbolShort) ? '' : 'alternate: ' + symbolShort;
+	intervals.textContent 		= (!chord) ? '-' : chord.normalized.intervals.join('-');
+	json.textContent 			= (!chord) ? '-' : JSON.stringify(chord, null, 4);
+
+	updatePianoKeys(chord);
+});
+
+
+
+// Custom piano keys
+// https://github.com/jutunen/custom-piano-keys
+const pianokeysDiv = document.querySelector('[data-output="pianokeys"]');
+
 let mappedNotes;
 let markedKeys;
 let pianokeys;
@@ -30,14 +55,10 @@ function myReducer(total, item) {
 	return total + ' ' + item;
 }
 
-chordInput.addEventListener('keyup', () => {
-
+function updatePianoKeys() {
 	if(pianokeysDiv.firstChild) {
 		pianokeysDiv.firstChild.remove();
 	}
-
-	input = chordInput.value;
-	chord = parseChord(input);
 	if(chord) {
 		rootNoteIndex = rootNotes.indexOf(chord.normalized.rootNote);
 		if(rootNoteIndex !== -1) {
@@ -53,18 +74,4 @@ chordInput.addEventListener('keyup', () => {
 			pianokeysDiv.appendChild(pianokeys);
 		}
 	}
-
-	symbolNormalized = (!chord) ? '' : renderChordNormalized(chord);
-	symbolShort = (!chord) ? '' : renderChordShort(chord);
-
-	if (!chord) {
-		outputDetails.classList.add('hidden');
-	} else {
-		outputDetails.classList.remove('hidden');
-	}
-
-	normalized.textContent 		= (!chord) ? '-' : symbolNormalized;
-	normalizedShort.textContent = (symbolNormalized === symbolShort) ? '' : 'alternate: ' + symbolShort;
-	intervals.textContent 		= (!chord) ? '-' : chord.normalized.intervals.join('-');
-	json.textContent 			= (!chord) ? '-' : JSON.stringify(chord, null, 4);
-});
+}
