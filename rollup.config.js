2c1acc9691df82ddec6c9b68b63eab54338f75df
Christophe Noël <touffi51@gmail.com>
2020-08-26 22:42:06 +0200

    docs: Add notice on the usage of rollup at the top of the rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 /* eslint-env node */
+// Purpose of rollup is only to create the ESM bundle (chord-symbol.mjs) since as of today,
+// webpack does not seems to support this yet
 const path = require('path');
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
