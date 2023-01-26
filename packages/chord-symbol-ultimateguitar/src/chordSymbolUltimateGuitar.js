bacbcd74927a4ebfe111f7249b0459f1e05eabe0
Darryl Noakes <darryl.noakes@gmail.com>
2023-01-26 11:51:48 -0500

    Describe and type the Ultimate Guitar filter factory function

--- packages/chord-symbol-ultimateguitar/src/chordSymbolUltimateGuitar.js
+++ packages/chord-symbol-ultimateguitar/src/chordSymbolUltimateGuitar.js
@@ -1,3 +1,7 @@
+/**
+ * Create a renderer filter function that modifies the rendered chord to be closer to the style used by Ultimate Guitar.
+ * @returns {import("../../chord-symbol").CustomFilter} The filter function
+ */
 const chordSymbolUltimateGuitar = () => {
 	return (chord) => {
 		chord.formatted.symbol = chord.formatted.symbol
