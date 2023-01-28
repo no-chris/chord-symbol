3ccaf4467fad7391dff81e8b1b57774e456b0f43
Christophe Noël <touffi51@gmail.com>
2023-01-28 22:49:21 +0100

    refactor various filter by moving common datasets to shared dictionaries

--- packages/chord-symbol/src/parser/filters/formatNumeralSymbol.js
+++ packages/chord-symbol/src/parser/filters/formatNumeralSymbol.js
@@ -1,79 +1,7 @@
-import { getScaleAccidental } from '../../dictionaries/scales';
+import { flatsToSharps, notesSharp } from '../../dictionaries/notes';
+import { semitonesToDegree } from '../../dictionaries/degrees';
 import { minorQualities } from '../../dictionaries/qualities';
-import _invert from 'lodash/invert';
-
-// fixme: import from somewhere else
-const sharpsToFlats = {
-	'C#': 'Db',
-	'D#': 'Eb',
-	'F#': 'Gb',
-	'G#': 'Ab',
-	'A#': 'Bb',
-};
-const flatsToSharps = _invert(sharpsToFlats);
-
-const semitonesToDegree = {
-	major: {
-		sharp: {
-			0: 'I',
-			1: '♯I',
-			2: 'II',
-			3: '♯II',
-			4: 'III',
-			5: 'IV',
-			6: '♯IV',
-			7: 'V',
-			8: '♯V',
-			9: 'VI',
-			10: '♯VI',
-			11: 'VII',
-		},
-		flat: {
-			0: 'I',
-			1: '♭II',
-			2: 'II',
-			3: '♭III',
-			4: 'III',
-			5: 'IV',
-			6: '♭V',
-			7: 'V',
-			8: '♭VI',
-			9: 'VI',
-			10: '♭VII',
-			11: 'VII',
-		},
-	},
-	minor: {
-		sharp: {
-			0: 'I',
-			1: '♯I',
-			2: 'II',
-			3: 'III',
-			4: '♯III',
-			5: 'IV',
-			6: '♯IV',
-			7: 'V',
-			8: 'VI',
-			9: '♯VI',
-			10: 'VII',
-			11: '♯VII',
-		},
-		flat: {
-			0: 'I',
-			1: '♭II',
-			2: 'II',
-			3: 'III',
-			4: '♭IV',
-			5: 'IV',
-			6: '♭V',
-			7: 'V',
-			8: 'VI',
-			9: '♭VII',
-			10: 'VII',
-			11: '♭I',
-		},
-	},
-};
+import { getScaleAccidental } from '../../dictionaries/scales';
 
 /**
  * xxx
@@ -98,11 +26,13 @@ function isMinor(chord) {
 }
 
 function getRomanDegree(key, chord) {
+	const keyNote = key.replace('m', '');
+	const keyQuality = key.indexOf('m') > -1 ? 'minor' : 'major';
+
 	const interval = getIntervalBetweenNotes(
-		key.replace('m', ''),
+		keyNote,
 		chord.normalized.rootNote
 	);
-	const keyQuality = key.indexOf('m') > -1 ? 'minor' : 'major';
 	const accidental = getScaleAccidental(key);
 	const romanDegree = semitonesToDegree[keyQuality][accidental][interval];
 
@@ -110,21 +40,7 @@ function getRomanDegree(key, chord) {
 }
 
 function getIntervalBetweenNotes(note1, note2) {
-	const allNotes = [
-		'C',
-		'C#',
-		'D',
-		'D#',
-		'E',
-		'F',
-		'F#',
-		'G',
-		'G#',
-		'A',
-		'A#',
-		'B',
-	];
-	const note1Index = allNotes.indexOf(flatsToSharps[note1] || note1);
-	const note2Index = allNotes.indexOf(flatsToSharps[note2] || note2);
+	const note1Index = notesSharp.indexOf(flatsToSharps[note1] || note1);
+	const note2Index = notesSharp.indexOf(flatsToSharps[note2] || note2);
 	return (note2Index - note1Index + 12) % 12;
 }
