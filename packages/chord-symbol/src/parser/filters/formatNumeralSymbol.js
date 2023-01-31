c30707c136b0083fbc2aaa3499675beb4e29ac1e
Christophe Noël <touffi51@gmail.com>
2023-01-31 22:06:48 +0100

    refine chords to degrees conversions rules: detect diatonic and borrowed chords

--- packages/chord-symbol/src/parser/filters/formatNumeralSymbol.js
+++ packages/chord-symbol/src/parser/filters/formatNumeralSymbol.js
@@ -1,7 +1,20 @@
 import { flatsToSharps, notesSharp } from '../../dictionaries/notes';
 import { semitonesToDegree } from '../../dictionaries/degrees';
-import { minorQualities } from '../../dictionaries/qualities';
-import { getScaleAccidental } from '../../dictionaries/scales';
+import { minorQualities, qualities } from '../../dictionaries/qualities';
+
+const diatonicChords = {
+	major: ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'vii°'],
+	dom7: ['IΔ', 'ii⁷', 'iii⁷', 'IVΔ', 'V⁷', 'vi⁷', 'viiø⁷'],
+	minor: ['i', 'ii°', 'III', 'iv', 'v', 'VI', 'VII'],
+	minor7: ['i⁷', 'iiø⁷', 'IIIΔ', 'iv⁷', 'v⁷', 'VIΔ', 'VIIΔ'],
+};
+
+const borrowedChords = {
+	borrowedFromMinor: ['i', 'ii°', '♭III', 'iv', 'v', '♭VI', '♭VII'],
+	borrowedFromMinor7: ['i⁷', 'iiø⁷', '♭IIIΔ', 'iv⁷', 'v⁷', '♭VIΔ', '♭VIIΔ'],
+	borrowedFromMajor: ['I', 'ii', '♯iii', 'IV', 'V', '♯vi', '♯vii°'],
+	borrowedFromDom7: ['IΔ', 'ii⁷', '♯iii⁷', 'IVΔ', 'V⁷', '♯vi⁷', '♯viiø⁷'],
+};
 
 /**
  * xxx
@@ -9,38 +22,108 @@ import { getScaleAccidental } from '../../dictionaries/scales';
  * @param {Chord} chord
  * @returns {Chord}
  */
-export default function formatNumeralSymbol(key, chord) {
-	let numeralSymbol;
+export default function formatNumeralSymbol(key = '', chord) {
+	let degree;
+	let symbol;
+	let type;
 
-	if (!key) {
-		numeralSymbol = isMinor(chord) ? 'i' : 'I';
+	if (!key) key = chord.normalized.rootNote;
+
+	const keyQuality = key.indexOf('m') > -1 ? 'minor' : 'major';
+	degree = getRomanDegree(key, keyQuality, chord);
+
+	const descriptor = qualityToDescriptor[chord.normalized.quality](chord);
+
+	symbol = `${degree}${descriptor}`;
+
+	if (isDiatonic(keyQuality, symbol)) {
+		type = 'diatonic';
+	} else if (isBorrowed(keyQuality, symbol)) {
+		type = 'borrowed';
 	} else {
-		numeralSymbol = getRomanDegree(key, chord);
+		type = 'unknown';
+		degree = '?';
+		symbol = `${degree}${descriptor}`;
 	}
-	chord.formatted.numeralSymbol = numeralSymbol;
+
+	const inversion = '';
+
+	chord.numeral = {
+		symbol,
+		degree,
+		descriptor,
+		inversion,
+		type,
+	};
 	return chord;
 }
 
-function isMinor(chord) {
-	return minorQualities.includes(chord.normalized.quality);
-}
-
-function getRomanDegree(key, chord) {
+function getRomanDegree(key, keyQuality, chord) {
 	const keyNote = key.replace('m', '');
-	const keyQuality = key.indexOf('m') > -1 ? 'minor' : 'major';
 
 	const interval = getIntervalBetweenNotes(
 		keyNote,
 		chord.normalized.rootNote
 	);
-	const accidental = getScaleAccidental(key);
-	const romanDegree = semitonesToDegree[keyQuality][accidental][interval];
+	const romanDegree = semitonesToDegree[keyQuality][interval] || '?';
 
-	return isMinor(chord) ? romanDegree.toLowerCase() : romanDegree;
+	const chordQuality = minorQualities.includes(chord.normalized.quality)
+		? 'minor'
+		: 'major';
+
+	return chordQuality === 'minor' ? romanDegree.toLowerCase() : romanDegree;
 }
 
 function getIntervalBetweenNotes(note1, note2) {
 	const note1Index = notesSharp.indexOf(flatsToSharps[note1] || note1);
 	const note2Index = notesSharp.indexOf(flatsToSharps[note2] || note2);
 	return (note2Index - note1Index + 12) % 12;
+}
+
+const qualityToDescriptor = {
+	[qualities.ma]: () => '',
+	[qualities.ma6]: () => '',
+	[qualities.ma7]: () => 'Δ',
+	[qualities.dom7]: () => '⁷',
+
+	[qualities.mi]: () => '',
+	[qualities.mi6]: () => '',
+	[qualities.mi7]: (chord) =>
+		chord.normalized.intervals.includes('b5') ? 'ø⁷' : '⁷',
+	[qualities.miMa7]: () => 'mΔ',
+
+	[qualities.aug]: () => '+',
+	[qualities.dim]: () => '°',
+	[qualities.dim7]: () => '°⁷',
+
+	[qualities.power]: () => '',
+	[qualities.bass]: () => '',
+};
+
+function isDiatonic(keyQuality, symbol) {
+	if (keyQuality === 'major') {
+		return (
+			diatonicChords.major.includes(symbol) ||
+			diatonicChords.dom7.includes(symbol)
+		);
+	} else {
+		return (
+			diatonicChords.minor.includes(symbol) ||
+			diatonicChords.minor7.includes(symbol)
+		);
+	}
+}
+
+function isBorrowed(keyQuality, symbol) {
+	if (keyQuality === 'major') {
+		return (
+			borrowedChords.borrowedFromMinor.includes(symbol) ||
+			borrowedChords.borrowedFromMinor7.includes(symbol)
+		);
+	} else {
+		return (
+			borrowedChords.borrowedFromMajor.includes(symbol) ||
+			borrowedChords.borrowedFromDom7.includes(symbol)
+		);
+	}
 }
--- packages/chord-symbol/tests/parser/filters/formatNumeralSymbol.spec.js
+++ packages/chord-symbol/tests/parser/filters/formatNumeralSymbol.spec.js
@@ -34,101 +34,106 @@ describe('formatNumeralSymbol', () => {
 	describe.each([
 		/* * /
 		/* */
+		// diatonic chords (major key)
+		['C', '(triad/diatonic)', 'C', { symbol: 'I', type: 'diatonic' }],
+		['C', '(triad/diatonic)', 'Dm', { symbol: 'ii', type: 'diatonic' }],
+		['C', '(triad/diatonic)', 'Em', { symbol: 'iii', type: 'diatonic' }],
+		['C', '(triad/diatonic)', 'F', { symbol: 'IV', type: 'diatonic' }],
+		['C', '(triad/diatonic)', 'G', { symbol: 'V', type: 'diatonic' }],
+		['C', '(triad/diatonic)', 'Am', { symbol: 'vi', type: 'diatonic' }],
+		['C', '(triad/diatonic)', 'B°', { symbol: 'vii°', type: 'diatonic' }],
 
-		[undefined, 'D', 'I'],
-		[undefined, 'F#', 'I'],
-		[undefined, 'Ab', 'I'],
+		['C', '(7th/diatonic)', 'CM7', { symbol: 'IΔ', type: 'diatonic' }],
+		['C', '(7th/diatonic)', 'Dm7', { symbol: 'ii⁷', type: 'diatonic' }],
+		['C', '(7th/diatonic)', 'Em7', { symbol: 'iii⁷', type: 'diatonic' }],
+		['C', '(7th/diatonic)', 'FM7', { symbol: 'IVΔ', type: 'diatonic' }],
+		['C', '(7th/diatonic)', 'G7', { symbol: 'V⁷', type: 'diatonic' }],
+		['C', '(7th/diatonic)', 'Am7', { symbol: 'vi⁷', type: 'diatonic' }],
+		['C', '(7th/diatonic)', 'Bø7', { symbol: 'viiø⁷', type: 'diatonic' }],
 
-		['C', 'C', 'I'],
-		['C', 'C#', '♭II'],
-		['C', 'Db', '♭II'],
-		['C', 'D', 'II'],
-		['C', 'D#', '♭III'],
-		['C', 'Eb', '♭III'],
-		['C', 'E', 'III'],
-		['C', 'F', 'IV'],
-		['C', 'F#', '♭V'],
-		['C', 'Gb', '♭V'],
-		['C', 'G', 'V'],
-		['C', 'G#', '♭VI'],
-		['C', 'Ab', '♭VI'],
-		['C', 'A', 'VI'],
-		['C', 'A#', '♭VII'],
-		['C', 'Bb', '♭VII'],
-		['C', 'B', 'VII'],
+		// borrowed chords from parallel minor key
+		['C', '(triad/borrowed)', 'Cm', { symbol: 'i', type: 'borrowed' }],
+		['C', '(triad/borrowed)', 'D°', { symbol: 'ii°', type: 'borrowed' }],
+		['C', '(triad/borrowed)', 'Eb', { symbol: '♭III', type: 'borrowed' }],
+		['C', '(triad/borrowed)', 'Fm', { symbol: 'iv', type: 'borrowed' }],
+		['C', '(triad/borrowed)', 'Gm', { symbol: 'v', type: 'borrowed' }],
+		['C', '(triad/borrowed)', 'Ab', { symbol: '♭VI', type: 'borrowed' }],
+		['C', '(triad/borrowed)', 'Bb', { symbol: '♭VII', type: 'borrowed' }],
 
-		['D#', 'C', 'VI'],
-		['D#', 'C#', '♯VI'],
-		['D#', 'Db', '♯VI'],
-		['D#', 'D', 'VII'],
-		['D#', 'D#', 'I'],
-		['D#', 'Eb', 'I'],
-		['D#', 'E', '♯I'],
-		['D#', 'F', 'II'],
-		['D#', 'F#', '♯II'],
-		['D#', 'Gb', '♯II'],
-		['D#', 'G', 'III'],
-		['D#', 'G#', 'IV'],
-		['D#', 'Ab', 'IV'],
-		['D#', 'A', '♯IV'],
-		['D#', 'A#', 'V'],
-		['D#', 'Bb', 'V'],
-		['D#', 'B', '♯V'],
+		['C', '(7th/borrowed)', 'Cm7', { symbol: 'i⁷', type: 'borrowed' }],
+		['C', '(7th/borrowed)', 'Dø7', { symbol: 'iiø⁷', type: 'borrowed' }],
+		['C', '(7th/borrowed)', 'EbΔ', { symbol: '♭IIIΔ', type: 'borrowed' }],
+		['C', '(7th/borrowed)', 'Fm7', { symbol: 'iv⁷', type: 'borrowed' }],
+		['C', '(7th/borrowed)', 'Gm7', { symbol: 'v⁷', type: 'borrowed' }],
+		['C', '(7th/borrowed)', 'AbΔ', { symbol: '♭VIΔ', type: 'borrowed' }],
+		['C', '(7th/borrowed)', 'BbΔ', { symbol: '♭VIIΔ', type: 'borrowed' }],
 
-		['Cm', 'C', 'I'],
-		['Cm', 'C#', '♭II'],
-		['Cm', 'Db', '♭II'],
-		['Cm', 'D', 'II'],
-		['Cm', 'D#', 'III'],
-		['Cm', 'Eb', 'III'],
-		['Cm', 'E', '♭IV'],
-		['Cm', 'F', 'IV'],
-		['Cm', 'F#', '♭V'],
-		['Cm', 'Gb', '♭V'],
-		['Cm', 'G', 'V'],
-		['Cm', 'G#', 'VI'],
-		['Cm', 'Ab', 'VI'],
-		['Cm', 'A', '♭VII'],
-		['Cm', 'A#', 'VII'],
-		['Cm', 'Bb', 'VII'],
-		['Cm', 'B', '♭I'],
+		// diatonic chords (minor key)
+		['Cm', '(triad/diatonic)', 'Cm', { symbol: 'i', type: 'diatonic' }],
+		['Cm', '(triad/diatonic)', 'D°', { symbol: 'ii°', type: 'diatonic' }],
+		['Cm', '(triad/diatonic)', 'Eb', { symbol: 'III', type: 'diatonic' }],
+		['Cm', '(triad/diatonic)', 'Fm', { symbol: 'iv', type: 'diatonic' }],
+		['Cm', '(triad/diatonic)', 'Gm', { symbol: 'v', type: 'diatonic' }],
+		['Cm', '(triad/diatonic)', 'Ab', { symbol: 'VI', type: 'diatonic' }],
+		['Cm', '(triad/diatonic)', 'Bb', { symbol: 'VII', type: 'diatonic' }],
 
-		['Dbm', 'Db', 'I'],
-		['Dbm', 'D', '♭II'],
-		['Dbm', 'D#', 'II'],
-		['Dbm', 'Eb', 'II'],
-		['Dbm', 'E', 'III'],
-		['Dbm', 'F', '♭IV'],
-		['Dbm', 'F#', 'IV'],
-		['Dbm', 'Gb', 'IV'],
-		['Dbm', 'G', '♭V'],
-		['Dbm', 'G#', 'V'],
-		['Dbm', 'Ab', 'V'],
-		['Dbm', 'A', 'VI'],
-		['Dbm', 'A#', '♭VII'],
-		['Dbm', 'Bb', '♭VII'],
-		['Dbm', 'B', 'VII'],
-		['Dbm', 'C', '♭I'],
-		['Dbm', 'C#', 'I'],
+		['Cm', '(7th/diatonic)', 'Cm7', { symbol: 'i⁷', type: 'diatonic' }],
+		['Cm', '(7th/diatonic)', 'Dø7', { symbol: 'iiø⁷', type: 'diatonic' }],
+		['Cm', '(7th/diatonic)', 'EbΔ', { symbol: 'IIIΔ', type: 'diatonic' }],
+		['Cm', '(7th/diatonic)', 'Fm7', { symbol: 'iv⁷', type: 'diatonic' }],
+		['Cm', '(7th/diatonic)', 'Gm7', { symbol: 'v⁷', type: 'diatonic' }],
+		['Cm', '(7th/diatonic)', 'AbΔ', { symbol: 'VIΔ', type: 'diatonic' }],
+		['Cm', '(7th/diatonic)', 'BbΔ', { symbol: 'VIIΔ', type: 'diatonic' }],
+
+		// borrowed chords from parallel major key
+		['Cm', '(triad/borrowed)', 'C', { symbol: 'I', type: 'borrowed' }],
+		['Cm', '(triad/borrowed)', 'Dm', { symbol: 'ii', type: 'borrowed' }],
+		['Cm', '(triad/borrowed)', 'Em', { symbol: '♯iii', type: 'borrowed' }],
+		['Cm', '(triad/borrowed)', 'F', { symbol: 'IV', type: 'borrowed' }],
+		['Cm', '(triad/borrowed)', 'G', { symbol: 'V', type: 'borrowed' }],
+		['Cm', '(triad/borrowed)', 'Am', { symbol: '♯vi', type: 'borrowed' }],
+		['Cm', '(triad/borrowed)', 'B°', { symbol: '♯vii°', type: 'borrowed' }],
+
+		['Cm', '(7th/borrowed)', 'CM7', { symbol: 'IΔ', type: 'borrowed' }],
+		['Cm', '(7th/borrowed)', 'Dm7', { symbol: 'ii⁷', type: 'borrowed' }],
+		['Cm', '(7th/borrowed)', 'Em7', { symbol: '♯iii⁷', type: 'borrowed' }],
+		['Cm', '(7th/borrowed)', 'FM7', { symbol: 'IVΔ', type: 'borrowed' }],
+		['Cm', '(7th/borrowed)', 'G7', { symbol: 'V⁷', type: 'borrowed' }],
+		['Cm', '(7th/borrowed)', 'Am7', { symbol: '♯vi⁷', type: 'borrowed' }],
+		['Cm', '(7th/borrowed)', 'Bø7', { symbol: '♯viiø⁷', type: 'borrowed' }],
+
+		// other chords
+		['C', '(triad/other)', 'C7', { symbol: '?⁷', type: 'unknown' }],
+		['C', '(triad/other)', 'C°', { symbol: '?°', type: 'unknown' }],
 
 		/* * /
 		/* */
-	])('Key of %s', (key, chord, expectedDegree) => {
+	])('Key of %s %s', (key, title, chord, expected) => {
+		test(`${chord} => ${expected.symbol}`, () => {
+			const parseChord = getParseChord();
+			const normalized = formatNumeralSymbol(key, parseChord(chord));
+			expect(normalized.numeral.symbol).toEqual(expected.symbol);
+			expect(normalized.numeral.type).toEqual(expected.type);
+		});
+	});
+
+	describe.skip.each([
+		['C', 'Gma', 'V'],
+		['C', 'G7', 'V7'],
+		['C', 'G9', 'V7'],
+		['C', 'G13', 'V7'],
+		['C', 'GM7', 'VM7'],
+		['C', 'GM9', 'VM7'],
+		['C', 'GM13', 'VM7'],
+		['C', 'G+', 'V+'],
+		['C', 'G7+', 'V+'],
+		['C', 'Gdim', 'v°'],
+		['C', 'Gdim7', 'v7°'],
+	])('Chord descriptor', (key, chord, expectedDegree) => {
 		test(`${chord} => ${expectedDegree}`, () => {
 			const parseChord = getParseChord();
 			const normalized = formatNumeralSymbol(key, parseChord(chord));
 			expect(normalized.formatted.numeralSymbol).toEqual(expectedDegree);
-		});
-
-		test(`${chord + 'm'} => ${expectedDegree.toLowerCase()}`, () => {
-			const parseChord = getParseChord();
-			const normalized = formatNumeralSymbol(
-				key,
-				parseChord(chord + 'm')
-			);
-			expect(normalized.formatted.numeralSymbol).toEqual(
-				expectedDegree.toLowerCase()
-			);
 		});
 	});
 });
