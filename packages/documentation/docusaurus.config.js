ad985c5e45474e2bc61b310b695c9663abe0d33f
Christophe Noël <touffi51@gmail.com>
2023-02-13 22:44:00 +0100

    removed default social card

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			// Replace with your project's social card
-			image: 'img/docusaurus-social-card.jpg',
+			//image: 'img/docusaurus-social-card.jpg',
 			navbar: {
 				title: '',
 				logo: {
